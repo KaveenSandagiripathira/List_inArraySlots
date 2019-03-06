3a6faa5fd0d7c6b37795a96febfcaf33ae51b216
Kaveen Sandagiripathira <ksandagiripathir@stuy.edu>
2019-03-06 13:33:22 -0500

    Template File Addition

---
+++ List_inArraySlots.java
@@ -0,0 +1,28 @@
+/*********************************
+Implementation of a List in Java
+ ********************************/
+
+public class List_inArraySlots {
+	//fields
+
+	//Constructors 
+    public List_inArraySlots() {
+	
+    }
+	
+	//To String
+    public String toString() {
+        return null;
+    }
+    
+	//Equals
+    public equals(Template other) {
+	return null;
+    }
+	
+	//Optional Main Method
+    public static void main(String[] commandLine) {
+	
+
+    }
+}
---
+++ UserOfList.java
@@ -0,0 +1,9 @@
+/*********************************
+Java file to test list
+ ********************************/
+
+public class UserOfList {
+	public static void main(String[] commandLine) {
+
+	}
+}
